d8f330d2b6544fc00a068c1751389a9b910f77b9
Adam Melcher <adam@newlinewebdesign.com>
2023-09-07 12:26:28 -0500

    refactored js files
    updated icon, added button margin, and open front end view in new tab

--- plugins/system/skeletonkey/media/js/backend.js
+++ plugins/system/skeletonkey/media/js/backend.js
@@ -4,56 +4,91 @@
  * @license   GPLv3 or later
  */
 
-"use strict";
-if (!window.Joomla) throw new Error("Joomla API was not properly initialised");
+'use strict';
 
-var initSkeletonKey = function() {
-	var a = Joomla.getOptions("plg_system_skeletonkey"),
-		b = document.querySelectorAll("table#userList>tbody tr");
-	if (!b || b.length < 1) return;
-	b.forEach(function(b) {
-		var c = b.querySelectorAll("td"),
-			d = 1 * c[c.length - 1].textContent;
-		if (-1 !== a.loginUsers.indexOf(d)) {
-			var e = b.querySelectorAll("th div.btn-group"),
-				f = e[0],
-				g = document.createElement("span");
-			g.classList.add("icon-external-link-alt", "me-1");
-			g.setAttribute("aria-hidden", "true");
-			var h = document.createElement("span");
-			h.textContent = Joomla.Text._("PLG_SYSTEM_SKELETONKEY_BTN_LABEL");
-			var i = document.createElement("button");
-			i.setAttribute("type", "button");
-			i.classList.add("btn", "btn-dark", "btn-sm");
-			i.appendChild(g);
-			i.appendChild(h);
-			i.addEventListener("click", function(a) {
-				a.preventDefault();
-				var b = Joomla.getOptions("system.paths"),
-					c = Joomla.getOptions("csrf.token"),
-					e = "".concat(b ? "".concat(b.base, "/index.php") : window.location.pathname, "?option=com_ajax&format=json&plugin=skeletonkey&group=system&user_id=%d").concat(c ? "&".concat(c, "=1") : "");
-				Joomla.request({
-					url: e.replace("%d", d.toString()),
-					onSuccess: function onSuccess(a) {
-						var c = JSON.parse(a).data;
-						if (c && c[0] !== false) {
-							window.open(b.rootFull, '_blank'); // Open in a new tab
-						} else {
-							Joomla.renderMessages({
-								error: [Joomla.Text._("PLG_SYSTEM_SKELETONKEY_ERR_LOGINFAILED")]
-							});
-						}
-					},
-					onError: function onError() {
-						Joomla.renderMessages({
-							error: [Joomla.Text._("PLG_SYSTEM_SKELETONKEY_ERR_LOGINFAILED_AJAX")]
-						});
-					}
-				});
-			});
-			f.appendChild(i);
+if (!window.Joomla)
+{
+	throw new Error('Joomla API was not properly initialised');
+}
+
+const initSkeletonKey = () => {
+	// Get the user list rows
+	const options = Joomla.getOptions('plg_system_skeletonkey');
+	const rows    = document.querySelectorAll('table#userList>tbody tr');
+
+	// No user rows? Nothing to do here.
+	if (!rows || rows.length < 1)
+	{
+		return;
+	}
+
+	// Iterate through all of the rows
+	rows.forEach((elRow) => {
+		// Get the user ID
+		const elCells = elRow.querySelectorAll('td');
+		const userId  = elCells[elCells.length - 1].textContent * 1;
+
+		// If we are not allowed to log in this user go away.
+		if (options.loginUsers.indexOf(userId) === -1)
+		{
+			return;
 		}
-	});
-};
-initSkeletonKey();
 
+		// Find the button group which has the Add Note button
+		const elButtonGroups = elRow.querySelectorAll('th div.btn-group');
+		const elButtonGroup  = elButtonGroups[0];
+
+		// Create the icon for the login button
+		const elIcon = document.createElement('span');
+		elIcon.classList.add('icon-external-link-alt', 'me-1');
+		elIcon.setAttribute('aria-hidden', 'true');
+
+		// Create the text for the login button
+		const elSpan       = document.createElement('span');
+		elSpan.textContent = Joomla.Text._('PLG_SYSTEM_SKELETONKEY_BTN_LABEL');
+
+		// Create the login button itseld
+		const elLink = document.createElement('button');
+		elLink.setAttribute('type', 'button');
+		elLink.classList.add('btn', 'btn-dark', 'btn-sm', 'ms-1');
+		elLink.appendChild(elIcon);
+		elLink.appendChild(elSpan);
+
+		// Create a special click event handler
+		elLink.addEventListener('click', (e) => {
+			e.preventDefault();
+
+			const paths = Joomla.getOptions('system.paths');
+			const token = Joomla.getOptions('csrf.token');
+			const uri   = `${paths ? `${paths.base}/index.php` : window.location.pathname}?option=com_ajax&format=json&plugin=skeletonkey&group=system&user_id=%d${token ? `&${token}=1` : ''}`;
+
+			Joomla.request({
+				url:       uri.replace('%d', userId.toString()),
+				onSuccess: (data, xhr) => {
+					const returnedData = JSON.parse(data).data;
+
+					if (!returnedData || returnedData[0] === false)
+					{
+						Joomla.renderMessages({
+							error: [Joomla.Text._('PLG_SYSTEM_SKELETONKEY_ERR_LOGINFAILED')]
+						});
+
+						return;
+					}
+
+					window.open(paths.rootFull, '_blank');
+				},
+				onError:   (xhr) => {
+					Joomla.renderMessages({
+						error: [Joomla.Text._('PLG_SYSTEM_SKELETONKEY_ERR_LOGINFAILED_AJAX')]
+					});
+				}
+			});
+		});
+
+		// Append the login button to the button group, after the Add Note button
+		elButtonGroup.appendChild(elLink);
+	});
+}
+
+initSkeletonKey();
